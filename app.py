b82df424b6008ef3c84f6a575e83c52771eb37b8
Ritika Yadav <ritikakumariyadav2022@gmail.com>
2026-07-28 15:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ GOOGLE_API_KEY =st.sidebar.text_input("Google API Key",type='password')
 GROQ_API_KEY =st.sidebar.text_input("GROQ API Key",type='password')
 TAVILY_API_KEY =st.sidebar.text_input("TAVILY API Key",type='password')
 
-if not (GOOGLE_API_KEY) and not (GROQ)_API_KEY) and not (TAVILY_API_KEY) :
+if not (GOOGLE_API_KEY) and not (GROQ_API_KEY) and not (TAVILY_API_KEY) :
     st.sidebar.warning("pass api keys")
     st.stop
 else:
